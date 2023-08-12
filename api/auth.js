fe5b5776b068fe6046ab4159480c53985bed0b1a
dahelo <dohernandez93@gmail.com>
2023-08-13 00:22:40 +0100

    refactor change callback URL for githubStrategy authentication

--- api/auth.js
+++ api/auth.js
@@ -21,7 +21,7 @@ module.exports = function (app, myDataBase) {
   passport.use(new GitHubStrategy({
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'https://advancednode.davidhelo.repl.co/auth/github/callback'
+      callbackURL: 'https://user-authetication-form.vercel.app/auth/github/callback'
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log(profile);
